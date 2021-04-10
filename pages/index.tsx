125c8e43dd736c498fbfbab2552daacfb5db974e
Eric Florenzano <floguy@gmail.com>
2021-04-09 16:36:58 -1000

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function Index() {
+  return (
+    <div>
+      <Head>
+        <title>Webgames IDE</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h2>Webgames IDE</h2>
+        <h3>Hello, World!</h3>
+      </main>
+    </div>
+  );
+}
