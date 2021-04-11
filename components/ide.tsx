3a67fa898836d7224dba372f5adbe142947e59f8
Eric Florenzano <floguy@gmail.com>
2021-04-10 15:10:57 -1000

    Switch to making the frontend data-driven rather than hardcoded

--- components/ide.tsx
+++ components/ide.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';
 
 import Editor from './ide/editor';
 import SceneChooser from './ide/scenechooser';
 import Objects from './ide/objects';
 import Assets from './ide/assets';
 import Console from './ide/console';
+import { makeDefaultSceneData, Scene } from './ide/data';
 
-const IDE = () => (
-  <div className="flex flex-col h-screen w-screen ">
-    <div className="flex flex-row flex-1">
-      <div className="flex flex-col flex-1 max-w-xs">
-        <SceneChooser
-          className="flex-none bg-purple-500"
-          sceneNames={['main']}
-          onChange={(name: string) => console.log(name)} />
-        <Objects className="flex-1 bg-yellow-500" />
-        <Assets className="flex-1 bg-red-500" />
+const IDE = () => {
+  const [sceneData, setSceneData] = useState(makeDefaultSceneData());
+  const scene = sceneData.scenes.find((value: Scene): Boolean => {
+    return value.name == sceneData.currentSceneName;
+  });
+  const onSceneChanged = useCallback((name: string) => {
+    console.log('onSceneChanged', name);
+    setSceneData({
+      scenes: sceneData.scenes,
+      currentSceneName: name,
+    });
+  }, [sceneData]);
+  return (
+    <div className="flex flex-col h-screen w-screen">
+      <div className="flex flex-row flex-1">
+        <div className="flex flex-col flex-1 max-w-xs">
+          <SceneChooser
+            className="flex-none bg-purple-500"
+            sceneNames={sceneData.scenes.map((scene: Scene) => scene.name)}
+            onChange={onSceneChanged} />
+          <Objects
+            className="flex-1 bg-yellow-500"
+            scene={scene} />
+          <Assets className="flex-1 bg-red-500" />
+        </div>
+        <Editor className="flex-1 bg-blue-500" />
       </div>
-      <Editor className="flex-1 bg-blue-500" />
+      <Console className="flex-none h-36 bg-green-500" />
     </div>
-    <Console className="flex-none h-36 bg-green-500" />
-  </div>
-);
+  );
+};
 
 export default IDE;
--- components/ide/objects.tsx
+++ components/ide/objects.tsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';
 import LayerChooser from './layerchooser';
+import { Scene, GameObject, Layer, Component } from './data';
 
 type Props = {
   className?: string
+  scene: Scene,
 }
 
-const Objects = ({ className }: Props) => (
-  <div className={className}>
-    <LayerChooser
-      className="flex-none"
-      layerNames={['obj', 'bg', 'ui']}
-      onChange={(name: string) => console.log(name)} />
-  </div>
-);
+const Objects = ({ className, scene }: Props) => {
+  if (!scene) {
+    return null;
+  }
+  const [layerIndex, setLayerIndex] = useState(0);
+  const gameObjects = scene.layers[layerIndex].gameObjects;
+  const onLayerChange = useCallback((name: string) => {
+    let nextIndex = scene.layers.findIndex((layer: Layer) => layer.name == name);
+    if (nextIndex >= 0) {
+      setLayerIndex(nextIndex);
+    }
+  }, [scene]);
+  return (
+    <div className={className}>
+      <LayerChooser
+        className="flex-none"
+        layerNames={scene.layers.map((layer: Layer) => layer.name)}
+        onChange={onLayerChange} />
+      <ul>
+        {gameObjects.map((gameObject: GameObject, i: number) => {
+          return <li key={i}>[{gameObject.components.map((comp: Component) => comp.type).join(', ')}]</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
 
 export default Objects;
