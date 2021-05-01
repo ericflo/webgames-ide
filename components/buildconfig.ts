8eec3827e3205e5f670653f647ffdd7d48b2c607
Eric Florenzano <floguy@gmail.com>
2021-05-01 11:33:42 -1000

    End with slash

--- components/buildconfig.ts
+++ components/buildconfig.ts
@@ -1,4 +1,4 @@
 export const isProd: boolean = process.env.NEXT_PUBLIC_IS_PROD === 't';
 export const isHandshake: boolean =
   process.env.NEXT_PUBLIC_IS_HANDSHAKE === 't';
-export const linkPortal: string = 'https://siasky.net';
+export const linkPortal: string = 'https://siasky.net/';
