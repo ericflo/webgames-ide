bf781c3eef5fcf4b0b1f7cb1594222154b55f9ba
Eric Florenzano <floguy@gmail.com>
2021-05-04 18:08:14 -1000

    Add basic move and scale on the editor player

--- pages/editorplayer.tsx
+++ pages/editorplayer.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import Head from 'next/head';
 
@@ -54,6 +54,54 @@ const Player = () => {
     sceneData,
     isPlaying,
   ]);
+
+  useEffect(() => {
+    if (!k) {
+      return;
+    }
+    const data = {
+      clicked: false,
+      startPos: k.vec2(0, 0),
+      camScale: 1,
+      camPos: k.vec2(window.innerWidth * 0.5, window.innerHeight * 0.5),
+    };
+    const handleMouseDown = (ev: MouseEvent) => {
+      //if (ev.button != 2) {
+      //  return;
+      //}
+      data.clicked = true;
+      data.startPos = k.mousePos();
+    };
+    const handleMouseMove = (ev: MouseEvent) => {
+      if (!data.clicked) {
+        return;
+      }
+      const pos = data.camPos.clone().sub(k.mousePos().sub(data.startPos));
+      k.camPos(pos);
+    };
+    const handleMouseUp = (ev: MouseEvent) => {
+      //if (ev.button != 2) {
+      //  return;
+      //}
+      data.clicked = false;
+      data.camPos = data.camPos.clone().sub(k.mousePos().sub(data.startPos));
+    };
+    const handleWheel = (ev: WheelEvent) => {
+      ev.preventDefault();
+      data.camScale += Math.max(ev.deltaY, ev.deltaX) * 0.01;
+      k.camScale(data.camScale);
+    };
+    window.addEventListener('mousedown', handleMouseDown);
+    window.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener('mouseup', handleMouseUp);
+    window.addEventListener('wheel', handleWheel);
+    return () => {
+      window.removeEventListener('mousedown', handleMouseDown);
+      window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('mouseup', handleMouseUp);
+      window.removeEventListener('wheel', handleWheel);
+    };
+  }, [k]);
 
   /*
   const resizeHandler = useCallback(() => {
